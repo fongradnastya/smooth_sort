fd6fc8e11e6f43c4008760ef40eb2bfb8ba6e7d8
fongradnastya <fongradnastya@gmail.com>
2022-11-27 21:53:34 +0700

    Changed functions structure

--- sort.py
+++ sort.py
@@ -9,9 +9,13 @@ def count_leonardo_numb(k):
     return count_leonardo_numb(k - 1) + count_leonardo_numb(k - 2) + 1
 
 
-def smooth_sort(array):
-    def create_heap(arr: list) -> list:
-        size_list = []
+def smooth_sort(array: list, key: Optional[Callable] = None,
+                cmp: Optional[Callable] = None) -> list:
+    key = key if key is not None else lambda x: x
+    cmp = cmp if cmp is not None else lambda x, y: x < y
+    size_list = []
+
+    def create_heap(arr: list) -> None:
         for heap_end in range(len(arr)):
             if not size_list:
                 size_list.append(1)
@@ -23,13 +27,50 @@ def smooth_sort(array):
                     size_list.append(0)
                 else:
                     size_list.append(1)
-            idx, size_idx = _fix_roots(arr, size_list,
+            idx, size_idx = fix_roots(arr, size_list,
                                        heap_end, len(size_list) - 1)
-            _sift_down(arr, idx, size_list[size_idx])
-        print(size_list)
-        return size_list
+            sift_down(arr, idx, size_list[size_idx])
 
-    size_list = create_heap(array)
+    def sift_down(heap: list, root_idx: int, tree_size: int,):
+        """Просеивание"""
+        cur = root_idx
+        while tree_size > 1:
+            right = cur - 1
+            left = cur - 1 - count_leonardo_numb(tree_size - 2)
+            if cmp(key(heap[left]), key(heap[cur])) and \
+                    cmp(key(heap[right]), key(heap[cur])):
+                break
+            elif cmp(key(heap[left]), key(heap[right])):
+                heap[cur], heap[right] = heap[right], heap[cur]
+                cur = right
+                tree_size = tree_size - 2
+            else:
+                heap[cur], heap[left] = heap[left], heap[cur]
+                cur = left
+                tree_size = tree_size - 1
+
+    def fix_roots(heap, sizes, start_heap_idx, start_size_idx):
+        """Добавление нового элемента"""
+        cur = start_heap_idx
+        size_cur = start_size_idx
+        while size_cur > 0:
+            next_i = cur - count_leonardo_numb(sizes[size_cur])
+            if cmp(key(heap[next_i]), key(heap[cur])):
+                break
+            if sizes[size_cur] > 1:
+                right = cur - 1
+                left = right - count_leonardo_numb(sizes[size_cur] - 2)
+                if cmp(key(heap[next_i]), key(heap[right])) or \
+                        cmp(key(heap[next_i]), key(heap[left])):
+                    break
+            temp = heap[cur]
+            heap[cur] = heap[next_i]
+            heap[next_i] = temp
+            size_cur = size_cur - 1
+            cur = next_i
+        return cur, size_cur
+
+    create_heap(array)
     for heap_size in range(len(array) - 1, -1, -1):
         print(array)
         removed_size = size_list.pop()
@@ -40,56 +81,13 @@ def smooth_sort(array):
             right_idx = heap_size - 1
             left_size_idx = len(size_list) - 2
             right_size_idx = len(size_list) - 1
-            idx, size_idx = _fix_roots(array, size_list, left_idx,
-                                       left_size_idx)
-            _sift_down(array, idx, size_list[size_idx])
-            idx, size_idx = _fix_roots(array, size_list, right_idx,
-                                       right_size_idx)
-            _sift_down(array, idx, size_list[size_idx])
+            idx, size_idx = fix_roots(array, size_list, left_idx,
+                                      left_size_idx)
+            sift_down(array, idx, size_list[size_idx])
+            idx, size_idx = fix_roots(array, size_list, right_idx,
+                                      right_size_idx)
+            sift_down(array, idx, size_list[size_idx])
     return array
-
-
-def _fix_roots(heap, sizes, start_heap_idx, start_size_idx):
-    """Добавление нового элемента"""
-    cur = start_heap_idx
-    size_cur = start_size_idx
-    while size_cur > 0:
-        next_i = cur - count_leonardo_numb(sizes[size_cur])
-        if heap[next_i] <= heap[cur]:
-            break
-        if sizes[size_cur] > 1:
-            right = cur - 1
-            left = right - count_leonardo_numb(sizes[size_cur] - 2)
-            if heap[next_i] <= heap[right] or heap[next_i] <= heap[left]:
-                break
-        temp = heap[cur]
-        heap[cur] = heap[next_i]
-        heap[next_i] = temp
-        size_cur = size_cur - 1
-        cur = next_i
-    return cur, size_cur
-
-
-def _sift_down(heap: list, root_idx: int, tree_size: int,
-               key: Optional[Callable] = None, cmp: Optional[Callable] = None):
-    """Просеивание"""
-    key = key if key is not None else lambda x: x
-    cmp = cmp if cmp is not None else lambda x, y: x > y
-    cur = root_idx
-    while tree_size > 1:
-        right = cur - 1
-        left = cur - 1 - count_leonardo_numb(tree_size - 2)
-        if cmp(key(heap[cur]), key(heap[left])) and \
-                cmp(key(heap[cur]), key(heap[right])):
-            break
-        elif cmp(key(heap[right]), key(heap[left])):
-            heap[cur], heap[right] = heap[right], heap[cur]
-            cur = right
-            tree_size = tree_size - 2
-        else:
-            heap[cur], heap[left] = heap[left], heap[cur]
-            cur = left
-            tree_size = tree_size - 1
 
 
 if __name__ == "__main__":
